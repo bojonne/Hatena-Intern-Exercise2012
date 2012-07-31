07c416a86446dcfcf711bdc7fa1a3e508c175b85
bojonne <bojisstone@gmail.com>
2012-08-01 06:22:50 +0900

    1st change

--- exercise2-1/template.js
+++ exercise2-1/template.js
@@ -1,9 +1,18 @@
 var Template = function(input) {
     // この関数を実装してください
+    var data = eval(input);
+    this.source = data.source;
 };
 
 Template.prototype = {
     render: function(variables) {
         // この関数を実装してください
+        var json = eval(variables);
+        var title = json.title;
+        var content = json.content;
+        
+        var html = this.source("{% title %}", title);
+        html = html.replace("{% content %}", content);
+        return html;
     }
 };
